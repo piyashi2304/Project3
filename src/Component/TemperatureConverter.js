a9e1b0a02bd90d3fefe63e8510acf5eef12c631c
unknown <piyashisaha@gmail.com>
2024-07-18 15:09:31 +0530

    handson3

---
+++ src/Component/TemperatureConverter.js
@@ -0,0 +1,40 @@
+// import React, { useState } from 'react'
+
+// function TemperatureConverter() {
+
+//     const [celsius, setCelsius] = useState('')
+//     const [fahrenheit, setFahrenheit] = useState("")
+
+//     const handleCelsiusChange = (e) => {
+//         const value = e.target.value;
+//         setCelsius(value);
+//         setFahrenheit(value !== '' ? (value * 9/5 + 32).toFixed(2) : '');
+//       };
+    
+//       const handleFahrenheitChange = (e) => {
+//         const value = e.target.value;
+//         setFahrenheit(value);
+//         setCelsius(value !== '' ? ((value - 32) * 5/9).toFixed(2) : '');
+//       };
+
+      
+//   return (
+//     <>
+//      <h1>Temperature Converter</h1>
+//       <div>
+//         <label>
+//           Celsius:
+//           <input type="number" value={celsius} onChange={handleCelsiusChange} />
+//         </label>
+//       </div>
+//       <div>
+//         <label>
+//           Fahrenheit:
+//           <input type="number" value={fahrenheit} onChange={handleFahrenheitChange} />
+//         </label>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default TemperatureConverter
